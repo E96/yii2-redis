2d1d5bc1959469dbd00bfcfd8dea1334125dcb9f
Vasily Ostanin <bazilio91@gmail.com>
2015-09-02 11:28:04 +0500

    fix hkeys -> hlen

--- ActiveQuery.php
+++ ActiveQuery.php
@@ -200,7 +200,7 @@ class ActiveQuery extends Component implements ActiveQueryInterface
                 $db = $modelClass::getDb();
             }
 
-            return sizeof($db->executeCommand('HKEYS', [$modelClass::keyPrefix()]));
+            return $db->executeCommand('HLEN', [$modelClass::keyPrefix()]);
         } else {
             return $this->executeScript($db, 'Count');
         }
